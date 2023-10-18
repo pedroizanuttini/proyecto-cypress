b39c96156f9b141870b3ded68c7df73571f81b1c
DESKTOP-79SKK4E\pedro <pedroizanuttini@gmail.com>
2023-10-18 16:07:31 +1100

    login page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
+import { LoginElements } from "./pages/login/login.elements"
+import { LoginMethods } from "./pages/login/login.methods"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login('username','password')
+    cy.wait(10000)
   })
 })
