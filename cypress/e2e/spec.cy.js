ce3623e0f20d4700f3522cc270ef34e10c66fd8b
DESKTOP-79SKK4E\pedro <pedroizanuttini@gmail.com>
2023-10-19 18:28:38 +1100

    Add logger

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,14 +3,25 @@ import { Homeelements } from "./pages/home/home.elements"
 import { HomeMethods } from "./pages/home/home.methods"
 import { LoginElements } from "./pages/login/login.elements"
 import { LoginMethods } from "./pages/login/login.methods"
+import { Logger } from "./util/logger"
 
 describe('template spec', () => {
   it('passes', () => {
-    // const usuario = 'random02'
-    // const contrasena = 'random02'
+    const usuario = 'random02'
+    const contrasena = 'random02'
+
+    Logger.stepNumber(1)
+    Logger.step('Navigate to Demoblaze page')
     cy.visit('https://www.demoblaze.com/index.html')
-    cy.wait(30000)
-    cartMethods.clickOnDeleteLink('Nokia lumia 1520')
-    cy.wait(3000)
+
+    Logger.stepNumber(2)
+    Logger.step('Click on "Login" link')
+    cy.get('a[data-target="#logInModal"]').click()
+
+    Logger.stepNumber(3)
+    Logger.step(`Login with this credentials: "${usuario}"/${contrasena}`)
+    LoginMethods.login(usuario,contrasena)
+    Logger.verification(`The Home should show "welcome ${usuario}" text`)
+    cy.get('a#nameofuser').should('contain.text',usuario)
   })
 })
