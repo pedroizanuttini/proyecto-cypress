8cb6f6db6782aed10852af33c9bd26d1b959f964
DESKTOP-79SKK4E\pedro <pedroizanuttini@gmail.com>
2023-10-19 11:19:51 +1100

    Create files for product details page

---
+++ cypress/e2e/pages/product-details/product-details.methods.js
@@ -0,0 +1,9 @@
+import { ProducDetailsElements } from "./product-datails.elements";
+
+export class ProducDetailsMethods{
+
+    static clickOnAddToCartButton(){
+        ProducDetailsElements.buttons.addToCart.click();
+    }
+}
+    
