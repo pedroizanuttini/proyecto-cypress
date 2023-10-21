9573253901dadc4e3680b0edcf89ec0e5b6d8cdc
DESKTOP-79SKK4E\pedro <pedroizanuttini@gmail.com>
2023-10-21 18:26:14 +1100

    Log in invalid user

--- cypress/e2e/tests/autenticacion.cy.js
+++ cypress/e2e/tests/autenticacion.cy.js
@@ -26,4 +26,26 @@ describe(CommonPageData.testSuites.authenticacion, () =>{
         Logger.verification('Verificar que se redirige al usuario a la página de inicio.')
         CommonPageMethods.verifySignedUser(LoginData.validCredentials.username)
     });
+
+    it('Inicio de sesion invalido', ()=>{
+
+        Logger.stepNumber(1)
+        Logger.step(' Navegar a la página de inicio.')
+        CommonPageMethods.navigateTodemoBlaze();
+
+        Logger.stepNumber(2)
+        Logger.step('Hacer clic en "Log in" en la barra de navegación.')
+        CommonPageMethods.clickOnLogin();
+
+        Logger.stepNumber(3)
+        Logger.step('Ingresar un nombre de usuario y contraseña invalidos.')
+        LoginMethods.insertUsername(LoginData.validCredentials.username)
+        LoginMethods.insertPassword('contrasenaInvalida')
+
+        Logger.stepNumber(4)
+        Logger.step('Hacer clic en "Log in" para iniciar sesión.')
+        LoginMethods.clickOnLoginButton();
+        Logger.verification('Verificar que se muestra un mensaje de error de inicio de sesion')
+        LoginMethods.verifyWrongPasswordMessage();
+    });
 });
